d3a91f9fc40f5610a8547879d16f7be856cc9fa6
rick-m-mcguire <rickmcguire@Ricks-MacBook-Pro.local>
2016-07-16 12:44:26 +1000

    Fixed ColorWave
    Unsure if issue in script of in QLC+

--- RGBScripts/ColorWave.js
+++ RGBScripts/ColorWave.js
@@ -3,167 +3,167 @@ var testAlgo;
 
 (
 
-    function()
-    {
-        var algo = new Object;
-        algo.apiVersion = 2;
-        algo.name = "ColorWave";
-        algo.author = "Rick McGuire";
-		    algo.acceptColors = 1; // 0 - No Colours, 1 - 1 Colour, 2 - 2 Colours
-        algo.properties = new Array();
+	function()
+	{
+		var algo = {};
+		algo.apiVersion = 2;
+		algo.name = "Color Wave";
+		algo.author = "Rick McGuire";
+		algo.acceptColors = 1; // 0 - No Colours, 1 - 1 Colour, 2 - 2 Colours
+		algo.properties = [];
 
-        /**
-         * Custom Property Definition
-         */
-        algo.HueRange = 50;
-        algo.properties.push("name:HueRange|type:range|display:Hue Range|values:0,360|write:setHueRange|read:getHueRange");
+		/**
+		* Custom Property Definition
+		*/
+		algo.HueRange = 120;
+		algo.properties.push("name:HueRange|type:range|display:Hue Range|values:0,360|write:setHueRange|read:getHueRange");
 
-        /**
-         * Custom Property Getter and Setter methods
-         */
-        algo.setHueRange = function(_preset)
-        {
-          algo.HueRange = _preset;
-        }
+		/**
+		* Custom Property Getter and Setter methods
+		*/
+		algo.setHueRange = function(setHueRangeValue)
+		{
+			algo.HueRange = setHueRangeValue;
+		};
 
-        algo.getHueRange = function()
-        {
-          return ""+algo.HueRange;
-        }
+		algo.getHueRange = function()
+		{
+			return ""+algo.HueRange;
+		};
 
-        algo.HueDirection = "Counter-Clockwise"; //TODO Both Counter-Clockwise Clockwise
-        algo.properties.push("name:HueDirection|type:list|display:Hue Direction|values:Clockwise,Both,Counter-Clockwise|write:setHueDirection|read:getHueDirection");
+		algo.HueDirection = "Clockwise";
+		algo.properties.push("name:HueDirection|type:list|display:Hue Direction|values:Clockwise,Both,Counter-Clockwise|write:setHueDirection|read:getHueDirection");
 
-        /**
-         * Custom Property Getter and Setter methods
-         */
-        algo.setHueDirection = function(_preset)
-        {
-          algo.HueDirection = _preset;
-        }
+		/**
+		* Custom Property Getter and Setter methods
+		*/
+		algo.setHueDirection = function(setHueDirectionValue)
+		{
+			algo.HueDirection = setHueDirectionValue;
+		};
 
-        algo.getHueDirection = function()
-        {
-          return ""+algo.HueDirection;
-        }
+		algo.getHueDirection = function()
+		{
+			return ""+algo.HueDirection;
+		};
 
 
-        /**
-         * The actual "algorithm" for this RGB script. Produces a map of
-         * size($width, $height) each time it is called.
-         *
-         * @param step The step number that is requested (0 to (algo.rgbMapStepCount - 1))
-         * @param rgb Tells the color requested by user in the UI.
-         * @return A two-dimensional array[height][width].
-         */
-        algo.rgbMap = function(width, height, rgb, step)
-        {
-          algo.NumSteps = width-1;
-          var HSV = QRgbToHSV(rgb);
-          //Create the HSV for the color
-          var h,s,v;
-          h = HSV.H;
-          s = HSV.S;
-          v = HSV.V;
+		/**
+		* The actual "algorithm" for this RGB script. Produces a map of
+		* size($width, $height) each time it is called.
+		*
+		* @param step The step number that is requested (0 to (algo.rgbMapStepCount - 1))
+		* @param rgb Tells the color requested by user in the UI.
+		* @return A two-dimensional array[height][width].
+		*/
+		algo.rgbMap = function(width, height, rgb, step)
+		{
+			var NumSteps = width-1;
+			var HSV = QRgbToHSV(rgb);
+			//Create the HSV for the color
+			var h,s,v;
+			h = HSV.H;
+			s = HSV.S;
+			v = HSV.V;
 
-          // Workout the Hues
-          var Hues = new Array (2*algo.NumSteps+1);
-          var firstHue, hueStep;
-          switch (algo.HueDirection) {
-            case "Clockwise":
-              firstHue = h;
-              hueStep = algo.HueRange/(2*algo.NumSteps+1);
-              break;
-            case "Counter-Clockwise":
-              firstHue = h;
-              hueStep = -algo.HueRange/(2*algo.NumSteps+1);
-              break;
-            default: //Both
-              firstHue = h-algo.HueRange;
-              hueStep = algo.HueRange/algo.NumSteps;
-              break;
-          }
+			// Workout the Hues
+			var Hues = new Array (2*NumSteps+1);
+			var firstHue, hueStep;
+			switch (algo.HueDirection) {
+				case "Clockwise":
+				firstHue = h;
+				hueStep = algo.HueRange/(2*NumSteps+1);
+				break;
+				case "Counter-Clockwise":
+				firstHue = h;
+				hueStep = -algo.HueRange/(2*NumSteps+1);
+				break;
+				case "Both":
+				firstHue = h-algo.HueRange;
+				hueStep = algo.HueRange/NumSteps;
+				break;
+			}
 
-          var i = 0;
-          for(i=0; i<2*algo.NumSteps+1;i++){
-            Hues[i]= firstHue+hueStep*i;
-          }
+			var i = 0;
+			for(i=0; i<2*NumSteps+1;i++){
+				Hues[i]= firstHue+hueStep*i;
+			}
 
-          //workout the starting pos for this step
-          var firstpos, pos, up;
-          i=0;
-          pos = 0;
-          up = true;
-          while (i!=step){
-            if (up){
-              pos++;
-              i++;
-            }
-            else {
-              pos--;
-              i++;
-            }
-            if (pos>=algo.NumSteps){
-              up = false;
-            }
-            if (pos<=-algo.NumSteps){
-              up = true;
-            }
-          }
-          firstpos = pos;
+			//workout the starting pos for this step
+			var firstpos, pos, up;
+			i=0;
+			pos = 0;
+			up = true;
+			while (i!=step){
+				if (up){
+					pos++;
+					i++;
+				}
+				else {
+					pos--;
+					i++;
+				}
+				if (pos>=NumSteps){
+					up = false;
+				}
+				if (pos<=-NumSteps){
+					up = true;
+				}
+			}
+			firstpos = pos;
 
-          //create empty map
-          var map = new Array(height);
-          for (var y = 0; y < height; y++)
-          {
-            map[y] = new Array();
-            pos = firstpos; //Initialise pos
-            up = true;
-            for (var x = 0; x < width; x++)
-            {
-              map[y][x] = HSVToQRgb(Hues[pos],s,v);
-              //map[y][x] = rgb;
-              //check direction
-              if (pos>=Hues.length-1){
-                up = false;
-              }
-              if (pos<=0){
-                up = true;
-              }
-              //new pos
-              if (up){
-                pos++;
-              }
-              else {
-                pos--;
-              }
-            }
-          }
-          return map;
-        }
+			//create empty map
+			var map = new Array(height);
+			for (var y = 0; y < height; y++)
+			{
+				map[y] = [];
+				pos = firstpos; //Initialise pos
+				up = true;
+				for (var x = 0; x < width; x++)
+				{
+					map[y][x] = HSVToQRgb(Hues[pos],s,v);
+					//map[y][x] = rgb;
+					//check direction
+					if (pos>=Hues.length-1){
+						up = false;
+					}
+					if (pos<=0){
+						up = true;
+					}
+					//new pos
+					if (up){
+						pos++;
+					}
+					else {
+						pos--;
+					}
+				}
+			}
+			return map;
+		};
 
-        /**
-         * Tells RGB Matrix how many steps this algorithm produces with size($width, $height)
-         *
-         * @param width The width of the map
-         * @param height The height of the map
-         * @return Number of steps required for a map of size($width, $height)
-         */
-        algo.rgbMapStepCount = function(width, height)
-        {
-            // All pixels in the map must be used exactly once, each one separately
-            // at a time. Therefore, the maximum number of steps produced by this
-            // script on a 5 * 5 grid is 25.
-            algo.NumSteps = width-1;
-            return 2*algo.NumSteps;
-			      //width * height;
-        }
+		/**
+		* Tells RGB Matrix how many steps this algorithm produces with size($width, $height)
+		*
+		* @param width The width of the map
+		* @param height The height of the map
+		* @return Number of steps required for a map of size($width, $height)
+		*/
+		algo.rgbMapStepCount = function(width, height)
+		{
+			// All pixels in the map must be used exactly once, each one separately
+			// at a time. Therefore, the maximum number of steps produced by this
+			// script on a 5 * 5 grid is 25.
+			var NumSteps = width-1;
+			return 2*NumSteps;
+			//width * height;
+		};
 
-        // Development tool access
-        testAlgo = algo;
+		// Development tool access
+		testAlgo = algo;
 
-        return algo;
-    }
+		return algo;
+	}
 )();
 
 /**
@@ -182,7 +182,7 @@ var testAlgo;
 * @returns a QRgb value for the color
 */
 function RGBToQRgb(r,g,b){
-  return (r << 16) + (g << 8) + b;
+	return (r << 16) + (g << 8) + b;
 }
 
 /**
@@ -193,41 +193,41 @@ function RGBToQRgb(r,g,b){
 * @returns an Object Containing {H,S,V} for the color
 */
 function RGBToHSV(r,g,b) {
-  //Values to return
-  var H, S, V;
+	//Values to return
+	var H, S, V;
 
-  var rp = r/255.0;
-  var gp = g/255.0;
-  var bp = b/255.0;
-  var cmax = Math.max(rp,Math.max(gp,bp));
-  var cmin = Math.min(rp,Math.min(gp,bp));
-  var delta = cmax - cmin;
-  var hue; // the hue placeholder
+	var rp = r/255.0;
+	var gp = g/255.0;
+	var bp = b/255.0;
+	var cmax = Math.max(rp,Math.max(gp,bp));
+	var cmin = Math.min(rp,Math.min(gp,bp));
+	var delta = cmax - cmin;
+	var hue; // the hue placeholder
 
-  //Calculate Hue
-  if (delta == 0){
-    hue = 0;
-  } else if (rp == cmax) {
-    hue = 60*((gp-bp)/delta);
-  } else if (gp == cmax) {
-    hue = 60*((bp-rp)/delta +2);
-  } else { // bp == cmax
-    hue = 60*((rp-gp)/delta +4);
-  }
+	//Calculate Hue
+	if (delta === 0){
+		hue = 0;
+	} else if (rp == cmax) {
+		hue = 60*((gp-bp)/delta);
+	} else if (gp == cmax) {
+		hue = 60*((bp-rp)/delta +2);
+	} else { // bp == cmax
+		hue = 60*((rp-gp)/delta +4);
+	}
 
-  H = ((hue+360) % 360); //Hue value to return
+	H = ((hue+360) % 360); //Hue value to return
 
-  //Calculate Saturation
-  if (cmax == 0){
-    S = 0;
-  } else {
-    S = delta / cmax;
-  }
+	//Calculate Saturation
+	if (cmax === 0){
+		S = 0;
+	} else {
+		S = delta / cmax;
+	}
 
-  //Calcuate Value
-  V = cmax;
+	//Calcuate Value
+	V = cmax;
 
-  return {H: H, S: S, V: V};
+	return {H: H, S: S, V: V};
 }
 
 //----------------- QRgb TO -----------------
@@ -238,16 +238,16 @@ function RGBToHSV(r,g,b) {
 * @returns an Object containing {Red, Green, Blue} for the color
 */
 function QRgbToRGB(QRgb){
-  // remove alpha chanel
-  QRgb = QRgb & 0x00ffffff;
-  // input validation
-  if(QRgb>0xFFFFFF){
-    QRgb=0x65CA7B;
-  }
-  if(QRgb<0){
-    QRgb=0;
-  }
-  return {Red: Math.round(((QRgb >> 16) & 0x00FF)), Green: Math.round(((QRgb >> 8) & 0x00FF)), Blue: Math.round((QRgb & 0x00FF))};
+	// remove alpha chanel
+	QRgb = QRgb & 0x00ffffff;
+	// input validation
+	if(QRgb>0xFFFFFF){
+		QRgb=0x65CA7B;
+	}
+	if(QRgb<0){
+		QRgb=0;
+	}
+	return {Red: Math.round(((QRgb >> 16) & 0x00FF)), Green: Math.round(((QRgb >> 8) & 0x00FF)), Blue: Math.round((QRgb & 0x00FF))};
 }
 
 /**
@@ -256,50 +256,50 @@ function QRgbToRGB(QRgb){
 * @returns an Object Containing {H,S,V} for the color
 */
 function QRgbToHSV(QRgb){
-  // remove alpha chanel
-  QRgb = QRgb & 0x00ffffff;
-  //input validation
-  if(QRgb>0xFFFFFF){
-    QRgb=0x6FDE7B; //Error Code 111 222 123
-  }
-  if(QRgb<0){
-    QRgb=0;
-  }
+	// remove alpha chanel
+	QRgb = QRgb & 0x00ffffff;
+	//input validation
+	if(QRgb>0xFFFFFF){
+		QRgb=0x6FDE7B; //Error Code 111 222 123
+	}
+	if(QRgb<0){
+		QRgb=0;
+	}
 
-  //Values to return
-  var H, S, V;
-  var rp = Math.round((QRgb >> 16) & 0x00FF)/255.0;
-  var gp = Math.round((QRgb >> 8) & 0x00FF)/255.0;
-  var bp = Math.round(QRgb & 0x00FF)/255.0;
-  var cmax = Math.max(rp,Math.max(gp,bp));
-  var cmin = Math.min(rp,Math.min(gp,bp));
-  var delta = cmax - cmin;
-  var hue; // the hue placeholder
+	//Values to return
+	var H, S, V;
+	var rp = Math.round((QRgb >> 16) & 0x00FF)/255.0;
+	var gp = Math.round((QRgb >> 8) & 0x00FF)/255.0;
+	var bp = Math.round(QRgb & 0x00FF)/255.0;
+	var cmax = Math.max(rp,Math.max(gp,bp));
+	var cmin = Math.min(rp,Math.min(gp,bp));
+	var delta = cmax - cmin;
+	var hue; // the hue placeholder
 
-  //Calculete Hue
-  if (delta == 0){
-    hue = 0;
-  } else if (rp == cmax) {
-    hue = 60*((gp-bp)/delta);
-  } else if (gp == cmax) {
-    hue = 60*((bp-rp)/delta +2);
-  } else { // bp == cmax
-    hue = 60*((rp-gp)/delta +4);
-  }
+	//Calculete Hue
+	if (delta === 0){
+		hue = 0;
+	} else if (rp == cmax) {
+		hue = 60*((gp-bp)/delta);
+	} else if (gp == cmax) {
+		hue = 60*((bp-rp)/delta +2);
+	} else { // bp == cmax
+		hue = 60*((rp-gp)/delta +4);
+	}
 
-  H = ((hue+360) % 360); //Hue value to return
+	H = ((hue+360) % 360); //Hue value to return
 
-  //Calculate Saturation
-  if (cmax == 0){
-    S = 0;
-  } else {
-    S = delta / cmax;
-  }
+	//Calculate Saturation
+	if (cmax === 0){
+		S = 0;
+	} else {
+		S = delta / cmax;
+	}
 
-  //Calcuate Value
-  V = cmax;
+	//Calcuate Value
+	V = cmax;
 
-  return {H: H, S: S, V: V};
+	return {H: H, S: S, V: V};
 }
 
 
@@ -314,75 +314,75 @@ function QRgbToHSV(QRgb){
 * @returns an Object containing {Red, Green, Blue} for the color
 */
 function HSVToRGB(h,s,v) {
-  //return values
-  var r, g, b;
+	//return values
+	var r, g, b;
 
-  //input validation
-  if (s>1){
-    s=1;
-  }
-  if(s<0){
-    s=0;
-  }
-  if (v>1){
-    v=1;
-  }
-  if(v<0){
-    v=0;
-  }
-  h = (h % 360 + 360) % 360; //Hue 360 wraps to 0
+	//input validation
+	if (s>1){
+		s=1;
+	}
+	if(s<0){
+		s=0;
+	}
+	if (v>1){
+		v=1;
+	}
+	if(v<0){
+		v=0;
+	}
+	h = (h % 360 + 360) % 360; //Hue 360 wraps to 0
 
-  var i, f, p, q, t;
+	var i, f, p, q, t;
 
-  if( s == 0 ) {
-    // achromatic (grey)
-    r = Math.round(v*255);
-    g = Math.round(v*255);
-    b = Math.round(v*255);
-  }
-  else {
-    // chroma (color)
-    var hue = h / 60;      // sector 0 to 5
-    i = Math.floor( hue );
-    f = hue - i;      // factorial part of hue
-    p = v * ( 1 - s );
-    q = v * ( 1 - s * f );
-    t = v * ( 1 - s * ( 1 - f ) );
+	if( s === 0 ) {
+		// achromatic (grey)
+		r = Math.round(v*255);
+		g = Math.round(v*255);
+		b = Math.round(v*255);
+	}
+	else {
+		// chroma (color)
+		var hue = h / 60;			// sector 0 to 5
+		i = Math.floor( hue );
+		f = hue - i;			// factorial part of hue
+		p = v * ( 1 - s );
+		q = v * ( 1 - s * f );
+		t = v * ( 1 - s * ( 1 - f ) );
 
-    switch( i ) {
-      case 0:
-      r = Math.round(v*255);
-      g = Math.round(t*255);
-      b = Math.round(p*255);
-      break;
-      case 1:
-      r = Math.round(q*255);
-      g = Math.round(v*255);
-      b = Math.round(p*255);
-      break;
-      case 2:
-      r = Math.round(p*255);
-      g = Math.round(v*255);
-      b = Math.round(t*255);
-      break;
-      case 3:
-      r = Math.round(p*255);
-      g = Math.round(q*255);
-      b = Math.round(v*255);
-      break;
-      case 4:
-      r = Math.round(t*255);
-      g = Math.round(p*255);
-      b = Math.round(v*255);
-      break;
-      default:    // case 5:
-      r = Math.round(v*255);
-      g = Math.round(p*255);
-      b = Math.round(q*255);
-      break;
-    }
-  }
-  return {Red: r, Green: g, Blue: b};
+		switch( i ) {
+			case 0:
+			r = Math.round(v*255);
+			g = Math.round(t*255);
+			b = Math.round(p*255);
+			break;
+			case 1:
+			r = Math.round(q*255);
+			g = Math.round(v*255);
+			b = Math.round(p*255);
+			break;
+			case 2:
+			r = Math.round(p*255);
+			g = Math.round(v*255);
+			b = Math.round(t*255);
+			break;
+			case 3:
+			r = Math.round(p*255);
+			g = Math.round(q*255);
+			b = Math.round(v*255);
+			break;
+			case 4:
+			r = Math.round(t*255);
+			g = Math.round(p*255);
+			b = Math.round(v*255);
+			break;
+			default:		// case 5:
+			r = Math.round(v*255);
+			g = Math.round(p*255);
+			b = Math.round(q*255);
+			break;
+		}
+	}
+	return {Red: r, Green: g, Blue: b};
 }
 
 /**
@@ -394,73 +394,73 @@ function HSVToRGB(h,s,v) {
 * @returns an Object containing {Red, Green, Blue} for the color
 */
 function HSVToQRgb(h,s,v) {
-  //return values
-  var r, g, b;
+	//return values
+	var r, g, b;
 
-  //input validation
-  if (s>1){
-    s=1;
-  }
-  if(s<0){
-    s=0;
-  }
-  if (v>1){
-    v=1;
-  }
-  if(v<0){
-    v=0;
-  }
-  h = (h % 360 + 360) % 360; //Hue 360 wraps to 0
+	//input validation
+	if (s>1){
+		s=1;
+	}
+	if(s<0){
+		s=0;
+	}
+	if (v>1){
+		v=1;
+	}
+	if(v<0){
+		v=0;
+	}
+	h = (h % 360 + 360) % 360; //Hue 360 wraps to 0
 
-  var i, f, p, q, t;
+	var i, f, p, q, t;
 
-  if( s == 0 ) {
-    // achromatic (grey)
-    r = Math.round(v*255);
-    g = Math.round(v*255);
-    b = Math.round(v*255);
-  }
-  else {
-    // chroma (color)
-    var hue = h / 60;      // sector 0 to 5
-    i = Math.floor( hue );
-    f = hue - i;      // factorial part of hue
-    p = v * ( 1 - s );
-    q = v * ( 1 - s * f );
-    t = v * ( 1 - s * ( 1 - f ) );
+	if( s === 0 ) {
+		// achromatic (grey)
+		r = Math.round(v*255);
+		g = Math.round(v*255);
+		b = Math.round(v*255);
+	}
+	else {
+		// chroma (color)
+		var hue = h / 60;			// sector 0 to 5
+		i = Math.floor( hue );
+		f = hue - i;			// factorial part of hue
+		p = v * ( 1 - s );
+		q = v * ( 1 - s * f );
+		t = v * ( 1 - s * ( 1 - f ) );
 
-    switch( i ) {
-      case 0:
-      r = Math.round(v*255);
-      g = Math.round(t*255);
-      b = Math.round(p*255);
-      break;
-      case 1:
-      r = Math.round(q*255);
-      g = Math.round(v*255);
-      b = Math.round(p*255);
-      break;
-      case 2:
-      r = Math.round(p*255);
-      g = Math.round(v*255);
-      b = Math.round(t*255);
-      break;
-      case 3:
-      r = Math.round(p*255);
-      g = Math.round(q*255);
-      b = Math.round(v*255);
-      break;
-      case 4:
-      r = Math.round(t*255);
-      g = Math.round(p*255);
-      b = Math.round(v*255);
-      break;
-      default:    // case 5:
-      r = Math.round(v*255);
-      g = Math.round(p*255);
-      b = Math.round(q*255);
-      break;
-    }
-  }
-  return (r << 16) + (g << 8) + b;
+		switch( i ) {
+			case 0:
+			r = Math.round(v*255);
+			g = Math.round(t*255);
+			b = Math.round(p*255);
+			break;
+			case 1:
+			r = Math.round(q*255);
+			g = Math.round(v*255);
+			b = Math.round(p*255);
+			break;
+			case 2:
+			r = Math.round(p*255);
+			g = Math.round(v*255);
+			b = Math.round(t*255);
+			break;
+			case 3:
+			r = Math.round(p*255);
+			g = Math.round(q*255);
+			b = Math.round(v*255);
+			break;
+			case 4:
+			r = Math.round(t*255);
+			g = Math.round(p*255);
+			b = Math.round(v*255);
+			break;
+			default:		// case 5:
+			r = Math.round(v*255);
+			g = Math.round(p*255);
+			b = Math.round(q*255);
+			break;
+		}
+	}
+	return (r << 16) + (g << 8) + b;
 }
