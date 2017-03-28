50dea5fc3c7e4d056335f4c10294bcd7380249a1
Rick McGuire <rick.m.mcguire@gmail.com>
2017-03-28 21:02:08 +1000

    Extended Mac Load to QLC script

--- Util/LoadToQLC.sh
+++ Util/LoadToQLC.sh
@@ -1,3 +1,5 @@
 echo Copy QLC-CBC to QLC+ working folder
 cp -r "/Users/rickmcguire/QLC-CBC/RGBScripts" "/Users/rickmcguire/Library/Application Support/QLC+/"
+cp -r "/Users/rickmcguire/QLC-CBC/Fixtures" "/Users/rickmcguire/Library/Application Support/QLC+/"
+cp -r "/Users/rickmcguire/QLC-CBC/InputProfiles" "/Users/rickmcguire/Library/Application Support/QLC+/"
 open -a QLC+
