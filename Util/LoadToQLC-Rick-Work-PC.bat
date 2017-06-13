caa8333dbaef9636f8b4746f327e1864c3adf843
Rick McGuire <rick.m.mcguire@gmail.com>
2017-06-13 14:29:51 +1000

    Fixed issue with Work Deployment File

--- Util/LoadToQLC-Rick-Work-PC.bat
+++ Util/LoadToQLC-Rick-Work-PC.bat
@@ -1,4 +1,4 @@
 ECHO Copying to To QLC from Working
-DEL "C:\Users\Tubby\QLC+\RGBScripts\*" /Q
+DEL "C:\Users\pmmcgui\QLC+\RGBScripts\*" /Q
 XCOPY "C:\SharedData\Apps\Git\QLC-CBC\*" "C:\Users\pmmcgui\QLC+\" /S /I /Y
 START "" "C:\SharedData\Apps\QLC+\qlcplus.exe"
