00d9dce6c248816b926a05b42093fecfba29bba6
Rick McGuire <rick.m.mcguire@gmail.com>
2017-11-05 10:24:41 +1000

    New Home PC change

--- Util/LoadToQLC-Rick-Home-PC.bat
+++ Util/LoadToQLC-Rick-Home-PC.bat
@@ -1,4 +1,5 @@
 ECHO Copying to To QLC from Working
 DEL "C:\Users\Tubby\QLC+\RGBScripts\*" /Q
-XCOPY "C:\Users\Tubby\Documents\GitHub\QLC-CBC\*" "C:\Users\Tubby\QLC+\" /S /I /Y
+XCOPY "C:\Users\Tubby\QLC-CBC\*" "C:\Users\Tubby\QLC+\" /S /I /Y
+PAUSE
 START "" "C:\QLC+\qlcplus.exe"
