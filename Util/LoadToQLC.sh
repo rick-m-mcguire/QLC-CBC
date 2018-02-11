a57c009c000ec043cad02b9f61b4baa3fa68cfb6
Rick McGuire <rick.m.mcguire@gmail.com>
2018-02-11 11:10:41 +1000

    Maintenance Mac Test Script

--- Util/LoadToQLC.sh
+++ Util/LoadToQLC.sh
@@ -1,5 +1,8 @@
 echo Copy QLC-CBC to QLC+ working folder
-cp -r "/Users/rickmcguire/QLC-CBC/RGBScripts" "/Users/rickmcguire/Library/Application Support/QLC+/"
-cp -r "/Users/rickmcguire/QLC-CBC/Fixtures" "/Users/rickmcguire/Library/Application Support/QLC+/"
-cp -r "/Users/rickmcguire/QLC-CBC/InputProfiles" "/Users/rickmcguire/Library/Application Support/QLC+/"
+rm -r "/Users/rickmcguire/Library/Application Support/QLC+/RGBScripts/"*
+rm -r "/Users/rickmcguire/Library/Application Support/QLC+/Fixtures/"*
+rm -r "/Users/rickmcguire/Library/Application Support/QLC+/InputProfiles/"*
+cp -r "/Users/rickmcguire/QLC-CBC/RGBScripts" "/Users/rickmcguire/Library/Application Support/QLC+"
+cp -r "/Users/rickmcguire/QLC-CBC/Fixtures" "/Users/rickmcguire/Library/Application Support/QLC+"
+cp -r "/Users/rickmcguire/QLC-CBC/InputProfiles" "/Users/rickmcguire/Library/Application Support/QLC+"
 open -a QLC+
